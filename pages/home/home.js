fdf80e2e9a6e9d843e60386b357fc13ec271f595
DandyChan <1026533423@qq.com>
2020-02-12 13:15:44 +0800

    知识点3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,23 @@
-// pages/home/home.js
 Page({
-
-	/**
-	 * 页面的初始数据
-	 */
-	data: {
-
+	data:{
+		name: 'Hello DandyChan',
+		age: 20,
+		students:[
+			{id: 1, name: 'chan', age:20},
+			{id: 2, name: 'chang', age:22},
+			{id: 3, name: 'zhang', age:24},
+			{id: 4, name: 'chuang', age:21},
+		],
+		counter: 0,
 	},
-
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad: function (options) {
-
+	handleBtnClick(){
+		this.setData({
+			counter: this.data.counter + 1
+		})
 	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
+	handleBtnSub(){
+		this.setData({
+			counter: this.data.counter - 1
+		})
 	}
 })
