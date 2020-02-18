bd5d02c118e4b3188f70781baf9ad99875d30001
DandyChan <1026533423@qq.com>
2020-02-18 16:52:23 +0800

    P32/P35_事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,69 @@
 // pages/home/home.js
 Page({
 
-	/**
-	 * 页面的初始数据
-	 */
 	data: {
-
+			titles: ['衣服', '裤子', '鞋子']
+	},
+	
+	handleBtnClick(){
+		console.log("button has click")
+	},
+	//常见事件
+	handleTouchStart(){
+		console.log("handleTouchStart")
+	},
+	handleTouchMove() {
+		console.log("handleTouchMove")
+	},
+	handleTouchEnd() {
+		console.log("handleTouchEnd")
+	},
+	handleTap() {
+		console.log("handleTap")
+	},
+	handleLongPress() {
+		console.log("handleLongPress")
 	},
 
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad: function (options) {
-
+	handleEventClick(event){
+		console.log("clickEvent", event)
+	},
+	handleEventEnd(event){
+		console.log("endEvent", event)
 	},
 
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
+	handleInner(event){
+		console.log(event)
+	},
+	handleOuter(event){
+		console.log(event)
 	},
 
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
+	handleItemClick(event){
+		console.log(event)
+		const dataset = event.currentTarget.dataset
+		const title = dataset.item
+		const index = dataset.index
+		console.log(title, index)
 	},
 
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
+	// 事件冒泡和事件捕获
+	handleCaptureView1(){
+		console.log("handleCaptureView1")
 	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
+	handleBindView1(){
+		console.log("handleBindView1")
 	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
+	handleCaptureView2(){
+		console.log("handleCaptureView2")
 	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
+	handleBindView2(){
+		console.log("handleBindView2")
 	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
-	}
+	handleCaptureView3(){
+		console.log("handleCaptureView3")
+	},
+	handleBindView3(){
+		console.log("handleBindView3")
+	},
 })
