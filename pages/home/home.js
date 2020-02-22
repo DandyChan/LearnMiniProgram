a030e3b8fd8483237e64ccce7f9b04371ca5e234
DandyChan <1026533423@qq.com>
2020-02-22 12:24:27 +0800

    P45/p46

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,6 @@
 // pages/home/home.js
+import request from '../../services/network.js'
+
 Page({
 
 	/**
@@ -12,55 +14,53 @@ Page({
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
+		// ---------1. 原生的方式发送网络请求---------
+		this.get_data_origin
 
+		//----------2. 使用封装的request发送网络请求---------
+		//promise最大好处就是防止出现回调地狱
+		request({
+			url: 'http://httpbin.org/get'
+		}).then(res => {
+			console.log(res)
+		}).catch(err => {
+			console.log(err)
+		})
 	},
 
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
+	get_data_origin(){
+		// //发送网络请求
+		// //1. 发送最简单的get请求
+		// wx.request({
+		// 	url: 'http://httpbin.org/get',
+		// 	success: function(res){
+		// 		console.log(res)
+		// 	}
+		// }),
 
-	},
+		// // 2. 发送带参数的get请求
+		// wx.request({
+		// 	url: 'http://httpbin.org/get',
+		// 	data: {
+		// 		name: "DandyChan",
+		// 		age: 20
+		// 	},
+		// 	success: function(res){
+		// 		console.log(res)
+		// 	}
+		// })
 
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
+		// 3. 发送带参数的post请求
+		wx.request({
+			url: 'http://httpbin.org/post',
+			method: 'post',
+			data: {
+				name: "DC",
+				type: "post"
+			},
+			success: function (res) {
+				console.log(res)
+			}
+		})
 	}
 })
